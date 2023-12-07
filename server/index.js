87943e9127d11c64070cea93a15f2628a549db94
adunmoye peter <adunmoyepeter111@gmail.com>
2023-12-07 08:16:47 +0000

    edited index.js

--- server/index.js
+++ server/index.js
@@ -31,5 +31,9 @@ mongoose.connect(process.env.DB_CONNECT, {
 
 app.use('/', TodoItemRoute);
 
+app.get('/', (req, res) => {
+  res.json("hello")
+})
+
 // add port and connect to server
 app.listen(PORT, () => console.log(`Server connected on port ${PORT}`));
